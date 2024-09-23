72e0904165a16c5d7653ca37f29bbb9409a34bd8
RealMyrdn <140298622+RealMyrdn@users.noreply.github.com>
2024-09-23 15:10:34 +0200

    Add files via upload

--- methods/forms.class.php
+++ methods/forms.class.php
@@ -4,24 +4,33 @@ class forms {
 //    function __construct() {
 //
 //    }
-    static function regNewUser () {
+    static function regNewUser() {
+        $i=0;
+        $fields[]='Vorname, vorname, text, Nachname, nachname, text, Strasse, strasse, text, PLZ, plz, text, Email, email, email, Passwort, password, password, Passwort wiederholen, repassword, password';
         $output='<table><form action="register.php" method="POST">';
-        $output.='<tr><td><label for="Vorname">Vorname:</label></td>';
-        $output.='<td><input type="text" id="Vorname" name="vorname" required></td></tr>';
-        $output.='<tr><td><label for="Nachname">Nachname:</label></td>';
-        $output.='<td><input type="Nachname" id="Nachname" name="Nachname" required></td></tr>';
-        $output.='<tr><td><label for="Straße">Straße:</label></td>';
-        $output.='<td><input type="Straße" id="Straße" name="Straße" required></td></tr>';
-        $output.='<tr><td><label for="PLZ">PLZ:</label></td>';
-        $output.='<td><input type="PLZ" id="PLZ" name="PLZ" required></td></tr>';
-        $output.='<tr><td><label for="email">E-Mail:</label></td>';
-        $output.='<td><input type="email" id="email" name="email" required></td></tr>';
-        $output.='<tr><td><label for="password">Passwort:</label></td>';
-        $output.='<td><input type="password" id="password" name="password" required></td></tr>';
-        $output.='<tr><td><label for="repassword">Passwort wiederholen:</label></td>';
-        $output.='<td><input type="password" id="repassword" name="repassword" required></td></tr>';
-        $output.='<td><input type="submit" value="Registrieren"></td>';
-        $output.='</form></table>';
+        while($fields[$i]) {
+            $output.='<tr><td><label for="' . $fields[$i] . '">' . $fields[$i] . ':</td>';
+            $output.='<td><input type="' . $fields[$i+2] . '" id="' . $fields[$i] . '" name="' . $fields[$i+1] . '" required></td></tr>';
+        }
+
+        $output.='<td><input type="submit" value="Registrieren"></td></form></table>';
+        $output.='<td><input type="submit" value="Registrieren"></td></form></table>';
+
+        // $output.='<tr><td><label for="Vorname">Vorname:</label></td>';
+        // $output.='<td><input type="text" id="Vorname" name="vorname" required></td></tr>';
+        // $output.='<tr><td><label for="Nachname">Nachname:</label></td>';
+        // $output.='<td><input type="text" id="Nachname" name="Nachname" required></td></tr>';
+        // $output.='<tr><td><label for="Straße">Straße:</label></td>';
+        // $output.='<td><input type="text" id="Straße" name="Straße" required></td></tr>';
+        // $output.='<tr><td><label for="PLZ">PLZ:</label></td>';
+        // $output.='<td><input type="text" id="PLZ" name="PLZ" required></td></tr>';
+        // $output.='<tr><td><label for="email">E-Mail:</label></td>';
+        // $output.='<td><input type="email" id="email" name="email" required></td></tr>';
+        // $output.='<tr><td><label for="password">Passwort:</label></td>';
+        // $output.='<td><input type="password" id="password" name="password" required></td></tr>';
+        // $output.='<tr><td><label for="repassword">Passwort wiederholen:</label></td>';
+        // $output.='<td><input type="password" id="repassword" name="repassword" required></td></tr>';
+
         
         return $output;
     }
