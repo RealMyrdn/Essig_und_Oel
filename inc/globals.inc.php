b3eaed99f4f8f4b3be121c1b7fe5ec6ed27cf4d3
RealMyrdn <140298622+RealMyrdn@users.noreply.github.com>
2024-09-24 06:07:04 +0200

    Delete inc directory

--- inc/globals.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('fromindex') or die;
-include $_SERVER['DOCUMENT_ROOT']  . "/methods/inc/config.inc.php";
-define("TEMPL_PATH", $_SERVER["DOCUMENT_ROOT"] . '/templates/' . $template . '/');
-define("LANG_SET", $langSet);
-?>
